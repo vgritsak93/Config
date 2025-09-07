83ad8c14f7e773ebeae967048562ac08e35e1764
Vadim Gritsak <vgritsak93@gmail.com>
2025-09-07 01:07:01 -0700

    snapshot: 2025-09-07 01:07:01

--- Visual Studio/src/GSADUs.Revit.Commands/Logger.cs
+++
@@ -1,9 +0,0 @@
-namespace GSADUs.Core
-{
-  public static class Logger
-  {
-    public static void Info(string msg) { /* TODO: implement */ }
-    public static void Warn(string msg) { /* TODO: implement */ }
-    public static void Error(string msg) { /* TODO: implement */ }
-  }
-}
